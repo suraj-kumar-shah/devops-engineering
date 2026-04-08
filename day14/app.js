7a13fb7a444948dc2ab1151930406cde4f689e30
suraj-kumar-sah <surajshah72600@gmail.com>
2026-04-08 13:31:51 +0530

    add day14

---
+++ day14/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
+});
